342f51cb0053ed0c28539cff871c86d1645a7306
ferrancho-el-pancho <ferllafol@alu.edu.gva.es>
2024-02-19 10:44:07 +0100

    Ejercicios finalizados (calculadora con definiciones y DNI)

--- Ejercicios/calculadora.py
+++ Ejercicios/calculadora.py
@@ -1,3 +1,22 @@
+def calculadora(num1, num2, oper):
+
+    if oper == "*":
+        result = num1 * num2
+    
+    elif oper == "+":
+        result = num1 + num2
+   
+    elif oper == "-":
+        result = num1 - num2
+    
+    else:
+        result = num1 / num2
+    
+    return result
+    
+
+
+
 numero = int(input('Introduce el primer número de la operación: '))
 numero_2 = int(input('Introduce el segundo número de la operación: '))
 operacion = input('Introduce la operación aritmética (+, -, * o /): ')
@@ -6,17 +25,4 @@ while operacion not in ('*','+','-','/'):
     operacion = input('La operación aritmética que ha puesto no es correcta. Porfavor escriba otra: ')
 
 
-if operacion == "*":
-    result = numero * numero_2
-    
-elif operacion == "+":
-    result = numero + numero_2
-   
-elif operacion == "-":
-    result = numero - numero_2
-    
-else:
-    result = numero / numero_2
-    
-
-print(f'El resultado de la operación aritmética de {numero} {operacion} {numero_2} es: {result}')
+print(f'El resultado de la operación aritmética de {numero} {operacion} {numero_2} es: {calculadora(numero, numero_2, operacion)}')
